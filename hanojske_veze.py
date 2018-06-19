fd27191eb8faf7c78e1647742c660451f1cb209c
Voldy <voldanvoldy@gmail.com>
2018-06-19 20:20:33 +0200

    Ještě to musím dodělat --> skoro hotové ?

--- hanojske_veze.py
+++ hanojske_veze.py
@@ -1,10 +1,46 @@
-def Vez(n,z, do, s):
-    if n >= 1:
-        Vez(n-1,z,s,do)
-        Disk(z,do)
-        Vez(n-1,s,do,z)
+win = False
+pole = []
+def game_setup():
+      global pole
+      pocet = int(input("Počet disků:"))
+      for i in range(0, pocet):
+            pole.append([0,0,0])
+      I = 1
+      for a in pole:
+            a[0] = I
+            I += 1
 
-def Disk(z,d):
-    print("Přesuň disk",z,"do",d)
+def tah():
+      global pocet
+      pohyb = input("-->").split()
+      start = int(pohyb[0])
+      cil = int(pohyb[1])
+      print(start,cil)
 
-Vez(3,"A","B","C")
+      for y in range(0, pocet):
+            try:
+                  if pole[cil][y+1] == 0:
+                        pass
+                  else:
+                        pole[cil][y] =
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+      game_setup()
+      while not win:
+            tah()
+            if pole[0][2] == 1:
+                  win = True
+
+      print("Vyhrál jsi")
